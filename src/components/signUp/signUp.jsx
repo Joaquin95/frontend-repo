bd96121fdecb6ea36f93634a5f6fb3e229c0c913
Joaquin <Mintinvestments95@gmail.com>
2025-02-28 09:13:01 -0600

    Updated url in sign up

--- src/components/signUp/signUp.jsx
+++ src/components/signUp/signUp.jsx
@@ -26,7 +26,7 @@ const SignUp = () => {
 
     try {
       const API_URL = import.meta.env.VITE_API_URL;
-      const response = await fetch("http://localhost:5000/api/auth/signup", {
+      const response = await fetch(`${API_URL}/api/auth/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
